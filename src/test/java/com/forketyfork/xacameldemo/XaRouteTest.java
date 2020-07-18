22f5b786cb43492fd58af8686d3e4215cd7938d4
Sergei <forketyfork@icloud.com>
2020-07-18 19:46:46 +0200

    wip

--- src/test/java/com/forketyfork/xacameldemo/XaRouteTest.java
+++ src/test/java/com/forketyfork/xacameldemo/XaRouteTest.java
@@ -5,12 +5,14 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.jms.ConnectionFactory;
 import javax.sql.DataSource;
+
 import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.NotifyBuilder;
 import org.apache.camel.test.spring.CamelSpringBootRunner;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +36,9 @@ public class XaRouteTest {
 
     @Autowired
     private CamelContext camelContext;
+
+    @EndpointInject("activemq:TestQueue")
+    private ProducerTemplate queueProducer;
 
     @Autowired
     @Qualifier("nonXaJmsConnectionFactory")
@@ -60,7 +65,7 @@ public class XaRouteTest {
                 .whenFailed(3)
                 .create();
 
-        sendMessage("testtesttest", "TestQueue");
+        jmsTemplate.send("TestQueue", session -> session.createTextMessage("testtesttest"));
 
         assertThat(notify.matches(10, TimeUnit.SECONDS)).isTrue();
 
@@ -69,27 +74,42 @@ public class XaRouteTest {
         assertThat(messages).isEmpty();
     }
 
-    public void sendMessage(String message, String queueName) {
-        jmsTemplate.send(queueName, session -> session.createTextMessage(message));
-    }
-
     // this test it fails, since the message is not yet committed to the database in the XA transaction
     // when NotifyBuilder reports route completion
     @Test
     @Transactional
-    @Ignore
     public void testSuccessWrong() {
-
-
 
         NotifyBuilder notify = new NotifyBuilder(camelContext)
                 .from("activemq:TestQueue")
                 .whenCompleted(1)
                 .create();
 
-        sendMessage("testtestte", "TestQueue");
+        queueProducer.sendBody("testtestte");
 
-        assertThat(notify.matches(20, TimeUnit.SECONDS)).isTrue();
+        assertThat(notify.matches(10, TimeUnit.SECONDS)).isTrue();
+
+        List<String> messages = jdbcTemplate.queryForList("select contents from message", String.class);
+
+        assertThat(messages).hasSize(1);
+        assertThat(messages.get(0)).isEqualTo("testtestte");
+
+    }
+
+    // this test it fails, since the message is not yet committed to the database in the XA transaction
+    // when NotifyBuilder reports route completion
+    @Test
+    @Transactional
+    public void testSuccessStillWrong() {
+
+        NotifyBuilder notify = new NotifyBuilder(camelContext)
+                .from("activemq:TestQueue")
+                .whenCompleted(1)
+                .create();
+
+        jmsTemplate.send("TestQueue", session -> session.createTextMessage("testtestte"));
+
+        assertThat(notify.matches(10, TimeUnit.SECONDS)).isTrue();
 
         List<String> messages = jdbcTemplate.queryForList("select contents from message", String.class);
 
@@ -99,7 +119,7 @@ public class XaRouteTest {
     }
 
     @Test
-    @Transactional // you can also remove the @Transactional annotation here
+    @Transactional // you can also omit the @Transactional annotation here
     public void testSuccessRight() throws Exception {
 
         NotifyBuilder notify = new NotifyBuilder(camelContext)
@@ -119,7 +139,7 @@ public class XaRouteTest {
                                 }))
 
         );
-        sendMessage("testtestte", "TestQueue");
+        jmsTemplate.send("TestQueue", session -> session.createTextMessage("testtestte"));
 
         assertThat(notify.matches(20, TimeUnit.SECONDS)).isTrue();
         assertTrue(transactionLatch.await(5, TimeUnit.SECONDS));
