26dd23fa829ee8b89ddac15455a917784a4ffe49
Sergei Petunin <petunin@crxmarkets.com>
2022-10-05 18:19:09 +0200

    Removed unused imports

--- src/main/java/com/forketyfork/xacameldemo/XaCamelDemoApplication.java
+++ src/main/java/com/forketyfork/xacameldemo/XaCamelDemoApplication.java
@@ -2,8 +2,6 @@ package com.forketyfork.xacameldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class XaCamelDemoApplication {
